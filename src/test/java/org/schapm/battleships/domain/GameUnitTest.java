6896179386abbeba1f10cc6fa88748a641a40bb1
schapm <ozuhouz@pm.me>
2022-04-08 17:13:13 +0100

    TEST: Add gameUnitCreatesCorrectTypesOfShips() test

--- src/test/java/org/schapm/battleships/domain/GameUnitTest.java
+++ src/test/java/org/schapm/battleships/domain/GameUnitTest.java
@@ -12,6 +12,8 @@ public class GameUnitTest {
 
     private final static int EXPECTED_OCEAN_SIZE = 10;
     private final static int EXPECTED_SHIPS_SIZE = 3;
+    private final static int EXPECTED_NUM_OF_BATTLESHIPS = 2;
+    private final static int EXPECTED_NUM_OF_DESTROYERS = 1;
 
     GameUnit gameUnit;
 
@@ -47,6 +49,27 @@ public class GameUnitTest {
     public void gameUnitCreatesCorrectNumberOfShips() {
         ArrayList<Ship> ships = gameUnit.getShips();
         assertEquals(EXPECTED_SHIPS_SIZE, ships.size(), "Expected a size of " + EXPECTED_SHIPS_SIZE + " for ships. Instead, it is " + ships.size());
+    }
+
+    @Test
+    public void gameUnitCreatesCorrectTypesOfShips() {
+        ArrayList<Ship> ships = gameUnit.getShips();
+
+        int battleships = 0;
+        int destroyers = 0;
+
+        for (Ship ship : ships) {
+            if (ship instanceof Battleship) {
+                battleships++;
+            }
+
+            if (ship instanceof Destroyer) {
+                destroyers++;
+            }
+        }
+
+        assertEquals(EXPECTED_NUM_OF_BATTLESHIPS, battleships, "Expected " + EXPECTED_NUM_OF_BATTLESHIPS + " battleships. Instead, it is " + battleships);
+        assertEquals(EXPECTED_NUM_OF_DESTROYERS, destroyers, "Expected " + EXPECTED_NUM_OF_DESTROYERS + " destroyers. Instead, it is " + destroyers);
     }
 
 }
