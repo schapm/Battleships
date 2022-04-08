c2dc333717d6b16c5329334208dd1ee04b04c7de
schapm <ozuhouz@pm.me>
2022-04-08 15:26:34 +0100

    TEST: gameUnitPlayerOceanAndPlayerGuessesGridsExist() passes

--- src/main/java/org/schapm/battleships/domain/GameUnit.java
+++ src/main/java/org/schapm/battleships/domain/GameUnit.java
@@ -6,4 +6,20 @@ package org.schapm.battleships.domain;
 
 public class GameUnit {
 
+    private final Coordinate ocean;
+    private final Coordinate guesses;
+
+    public GameUnit() {
+        this.ocean = new Coordinate();
+        this.guesses = new Coordinate();
+    }
+
+    public Coordinate getOcean() {
+        return this.ocean;
+    }
+
+    public Coordinate getGuesses() {
+        return this.guesses;
+    }
+
 }
