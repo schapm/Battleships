e88d67d3870d080f3323f8f3b9ce4d291f0b5c51
schapm <ozuhouz@pm.me>
2022-04-08 14:26:32 +0100

    TEST: playersHaveOwnGameUnit() passes

--- src/main/java/org/schapm/battleships/game/Player.java
+++ src/main/java/org/schapm/battleships/game/Player.java
@@ -1,5 +1,7 @@
 package org.schapm.battleships.game;
 
+import org.schapm.battleships.domain.GameUnit;
+
 /**
  * @author schapm
  */
@@ -7,13 +9,19 @@ package org.schapm.battleships.game;
 public abstract class Player {
 
     private final String name;
+    private final GameUnit gameUnit;
 
     public Player(String name) {
         this.name = name;
+        this.gameUnit = new GameUnit();
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public GameUnit getGameUnit() {
+        return this.gameUnit;
     }
 
 }
