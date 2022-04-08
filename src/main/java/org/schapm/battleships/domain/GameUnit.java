d5026f0048745cb903f6036cdd07c2f5806326b1
schapm <ozuhouz@pm.me>
2022-04-08 16:53:16 +0100

    TEST: gameUnitOceanAndGuessesGridsAreMultiDimensionalAndNotNull() passes

--- src/main/java/org/schapm/battleships/domain/GameUnit.java
+++ src/main/java/org/schapm/battleships/domain/GameUnit.java
@@ -8,19 +8,29 @@ public class GameUnit {
 
     public final static int OCEAN_SIZE = 10;
 
-    private final Coordinate[] ocean;
-    private final Coordinate[] guesses;
+    private final Coordinate[][] ocean;
+    private final Coordinate[][] guesses;
 
     public GameUnit() {
-        this.ocean = new Coordinate[OCEAN_SIZE];
-        this.guesses = new Coordinate[OCEAN_SIZE];
+        this.ocean = new Coordinate[OCEAN_SIZE][OCEAN_SIZE];
+        this.guesses = new Coordinate[OCEAN_SIZE][OCEAN_SIZE];
+        setupGrid(ocean);
+        setupGrid(guesses);
     }
 
-    public Coordinate[] getOcean() {
+    private void setupGrid(Coordinate[][] grid) {
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid.length; col++) {
+                grid[row][col] = new Coordinate();
+            }
+        }
+    }
+
+    public Coordinate[][] getOcean() {
         return this.ocean;
     }
 
-    public Coordinate[] getGuesses() {
+    public Coordinate[][] getGuesses() {
         return this.guesses;
     }
 
