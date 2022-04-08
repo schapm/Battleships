884127bdf03bc76cf4f72cefb0ae3de0a3348476
schapm <ozuhouz@pm.me>
2022-04-08 13:41:04 +0100

    TEST: Add playersHaveAName() test

--- src/test/java/org/schapm/battleships/game/PlayerTest.java
+++ src/test/java/org/schapm/battleships/game/PlayerTest.java
@@ -1,6 +1,7 @@
 package org.schapm.battleships.game;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +17,12 @@ public class PlayerTest {
     public void setupClass() {
         humanPlayer = new HumanPlayer();
         computerPlayer = new ComputerPlayer();
+    }
+
+    @Test
+    public void playersHaveAName() {
+        assertEquals(humanPlayer.getName(), HUMAN_PLAYER_NAME, HUMAN_PLAYER_NAME + " and getName() are not the same");
+        assertEquals(computerPlayer.getName(), COMPUTER_PLAYER_NAME, COMPUTER_PLAYER_NAME + " and getName() are not the same");
     }
 
 }
