d45a036b86d7356c159ffe285990b20abd958efd
schapm <ozuhouz@pm.me>
2022-04-08 15:25:36 +0100

    TEST: Add gameUnitPlayerOceanAndPlayerGuessesGridsExist() test

--- src/test/java/org/schapm/battleships/domain/GameUnitTest.java
+++ src/test/java/org/schapm/battleships/domain/GameUnitTest.java
@@ -1,6 +1,7 @@
 package org.schapm.battleships.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,6 +12,14 @@ public class GameUnitTest {
     @BeforeEach
     public void setupClass() {
         gameUnit = new GameUnit();
+    }
+
+    @Test
+    public void gameUnitPlayerOceanAndPlayerGuessesGridsExist() {
+        assertNotNull(gameUnit.getOcean(), "Ocean grid should not be null");
+        assertNotNull(gameUnit.getGuesses(), "Guesses grid should not be null");
+
+        assertNotEquals(gameUnit.getGuesses(), gameUnit.getOcean(), "getGuesses() and getOcean() should not be the same");
     }
 
 }
