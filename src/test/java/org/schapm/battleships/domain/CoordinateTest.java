3d98e0f92851c77a9bf28578030d7cc70c922da0
schapm <ozuhouz@pm.me>
2022-04-08 17:29:44 +0100

    TEST: Add coordinateHasAnXAndYValue() test

--- src/test/java/org/schapm/battleships/domain/CoordinateTest.java
+++ src/test/java/org/schapm/battleships/domain/CoordinateTest.java
@@ -1,16 +1,22 @@
 package org.schapm.battleships.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CoordinateTest {
 
     Coordinate coordinate;
-    
+
     @BeforeEach
     public void setupClass() {
-        coordinate = new Coordinate();
+        coordinate = new Coordinate(0, 5);
     }
 
+    @Test
+    public void coordinateHasAnXAndYValue() {
+        assertEquals(0, coordinate.getX(), "Expected X-value of " + 0 + ". Instead, it is " + coordinate.getX());
+        assertEquals(5, coordinate.getY(), "Expected Y-value of " + 5 + ". Instead, it is " + coordinate.getY());
+    }
 }
