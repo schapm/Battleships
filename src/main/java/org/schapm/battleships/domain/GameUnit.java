43d1750f13bd5f1891796583696af6a95fd88cbd
schapm <ozuhouz@pm.me>
2022-04-08 15:53:00 +0100

    TEST: gameUnitOceanAndGuessesGridsAreTheCorrectSize() passes

--- src/main/java/org/schapm/battleships/domain/GameUnit.java
+++ src/main/java/org/schapm/battleships/domain/GameUnit.java
@@ -6,19 +6,21 @@ package org.schapm.battleships.domain;
 
 public class GameUnit {
 
-    private final Coordinate ocean;
-    private final Coordinate guesses;
+    public final static int OCEAN_SIZE = 10;
+
+    private final Coordinate[] ocean;
+    private final Coordinate[] guesses;
 
     public GameUnit() {
-        this.ocean = new Coordinate();
-        this.guesses = new Coordinate();
+        this.ocean = new Coordinate[OCEAN_SIZE];
+        this.guesses = new Coordinate[OCEAN_SIZE];
     }
 
-    public Coordinate getOcean() {
+    public Coordinate[] getOcean() {
         return this.ocean;
     }
 
-    public Coordinate getGuesses() {
+    public Coordinate[] getGuesses() {
         return this.guesses;
     }
 
