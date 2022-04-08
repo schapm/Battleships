29265108ba6de39cbbd1b6f4d05e2c62dc163548
schapm <ozuhouz@pm.me>
2022-04-08 17:22:43 +0100

    TEST: Add CoordinateTest class

---
+++ src/test/java/org/schapm/battleships/domain/CoordinateTest.java
@@ -0,0 +1,16 @@
+package org.schapm.battleships.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CoordinateTest {
+
+    Coordinate coordinate;
+    
+    @BeforeEach
+    public void setupClass() {
+        coordinate = new Coordinate();
+    }
+
+}
