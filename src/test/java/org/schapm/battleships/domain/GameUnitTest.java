a2e95a013d4537e80b2b2c4514808ce7c9ffb94e
schapm <ozuhouz@pm.me>
2022-04-08 15:11:00 +0100

    TEST: Add GameUnitTest class

---
+++ src/test/java/org/schapm/battleships/domain/GameUnitTest.java
@@ -0,0 +1,16 @@
+package org.schapm.battleships.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameUnitTest {
+
+    GameUnit gameUnit;
+
+    @BeforeEach
+    public void setupClass() {
+        gameUnit = new GameUnit();
+    }
+
+}
