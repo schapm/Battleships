8e11e88351eae410526bbbe1db925687e00e863b
schapm <ozuhouz@pm.me>
2022-04-11 16:37:18 +0100

    TEST: PlayerTest - computerPlayerGeneratesRandomGuess() passes

--- src/main/java/org/schapm/battleships/game/ComputerPlayer.java
+++ src/main/java/org/schapm/battleships/game/ComputerPlayer.java
@@ -1,5 +1,10 @@
 package org.schapm.battleships.game;
 
+import org.schapm.battleships.domain.Coordinate;
+import org.schapm.battleships.domain.GameUnit;
+
+import java.util.Random;
+
 /**
  * @author schapm
  */
@@ -9,4 +14,13 @@ public class ComputerPlayer extends Player {
     public ComputerPlayer(String name) {
         super(name);
     }
+
+    public Coordinate guess() {
+        Random random = new Random();
+        int x = random.nextInt(GameUnit.OCEAN_SIZE);
+        int y = random.nextInt(GameUnit.OCEAN_SIZE);
+
+        return new Coordinate(x, y);
+    }
+
 }
