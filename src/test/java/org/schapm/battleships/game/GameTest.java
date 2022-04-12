b97d77c41001635b099ffa65a1a350110c599999
schapm <ozuhouz@pm.me>
2022-04-12 18:30:58 +0100

    TEST: Add GameTest class

---
+++ src/test/java/org/schapm/battleships/game/GameTest.java
@@ -0,0 +1,17 @@
+package org.schapm.battleships.game;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameTest {
+
+    private Game game;
+
+    @BeforeEach
+    public void setUp() {
+        game = new Game();
+    }
+
+}
