5979262f9e942942367494e38ac4fdd9c7bed888
schapm <ozuhouz@pm.me>
2022-04-13 17:29:25 +0100

    TEST: GameTest - playerGridsCanBePrinted() passes

--- src/main/java/org/schapm/battleships/game/Game.java
+++ src/main/java/org/schapm/battleships/game/Game.java
@@ -1,5 +1,7 @@
 package org.schapm.battleships.game;
 
+import org.schapm.battleships.domain.Coordinate;
+
 /**
  * @author schapm
  */
@@ -19,6 +21,15 @@ public class Game {
     public boolean isWinner() {
         return player.getGameUnit().getShips().isEmpty()
                 || opponent.getGameUnit().getShips().isEmpty();
+    }
+
+    public void printGrid(Coordinate[][] grid) {
+        for (int row = 0; row < grid.length; row++) {
+            System.out.println();
+            for (int col = 0; col < grid.length; col++) {
+                System.out.print("[" + grid[row][col].getValue() + "]");
+            }
+        }
     }
 
 }
