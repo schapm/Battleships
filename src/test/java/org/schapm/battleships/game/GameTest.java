82ab4ff18565612eea215c115828f69a82a0f916
schapm <ozuhouz@pm.me>
2022-04-13 14:02:36 +0100

    TEST: Add gameHasPlayerAndOpponent() test

--- src/test/java/org/schapm/battleships/game/GameTest.java
+++ src/test/java/org/schapm/battleships/game/GameTest.java
@@ -14,4 +14,13 @@ public class GameTest {
         game = new Game();
     }
 
+    @Test
+    public void gameHasPlayerAndOpponent() {
+        assertNotNull(game.player, "Player object should not be null");
+        assertTrue(game.player instanceof HumanPlayer, game.player.getName() + " is not an instance of class 'HumanPlayer'");
+
+        assertNotNull(game.opponent, "Opponent object should not be null");
+        assertTrue(game.opponent instanceof ComputerPlayer, game.opponent.getName() + " is not an instance of class 'ComputerPlayer'");
+    }
+
 }
