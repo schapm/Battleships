be60050be8fdc82478dc24e6774871bcb5ca7bd0
schapm <ozuhouz@pm.me>
2022-05-13 15:51:50 +0100

    REFACTOR: Remove unused addCoordinate()

--- src/main/java/org/schapm/battleships/domain/ShipInterface.java
+++ src/main/java/org/schapm/battleships/domain/ShipInterface.java
@@ -9,7 +9,6 @@ public interface ShipInterface {
     int getLength();
     Ship.Orientation getOrientation();
     ArrayList<Coordinate> getCoordinates();
-    void addCoordinate(Coordinate coordinate);
     void addCoordinates(Coordinate[] coordinates);
     void removeCoordinate(Coordinate coordinateToRemove);
     boolean isShipSunk();
